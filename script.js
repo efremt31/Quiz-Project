3c5a2bf49870386c8f8286c6709da6b25f09f5ca
efcode <128084445+efremt31@users.noreply.github.com>
2023-10-18 16:56:17 -0400

    first

---
+++ script.js
@@ -0,0 +1,121 @@
+const startButton = document.getElementById("start-btn");
+const nextButton = document.getElementById("next-btn");
+const questionContainerElement = document.getElementById("question-container");
+const questionElement = document.getElementById("question");
+const answerButtonsElement = document.getElementById("answer-buttons");
+
+let shuffledQuestions, currentQuestionIndex;
+
+startButton.addEventListener("click", startGame);
+nextButton.addEventListener("click", () => {
+  currentQuestionIndex++;
+  setNextQuestion();
+});
+
+function startGame() {
+  startButton.classList.add("hide");
+  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+  currentQuestionIndex = 0;
+  questionContainerElement.classList.remove("hide");
+  setNextQuestion();
+}
+
+function setNextQuestion() {
+  resetState();
+  showQuestion(shuffledQuestions[currentQuestionIndex]);
+}
+
+function showQuestion(question) {
+  questionElement.src = question.question;
+  question.answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.innerText = answer.text;
+    button.classList.add("btn");
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
+    answerButtonsElement.appendChild(button);
+  });
+}
+
+function resetState() {
+  clearStatusClass(document.body);
+  nextButton.classList.add("hide");
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild);
+  }
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target;
+  const correct = selectedButton.dataset.correct;
+  setStatusClass(document.body, correct);
+  Array.from(answerButtonsElement.children).forEach((button) => {
+    setStatusClass(button, button.dataset.correct);
+  });
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove("hide");
+  } else {
+    startButton.innerText = "Restart";
+    startButton.classList.remove("hide");
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element);
+  if (correct) {
+    element.classList.add("correct");
+  } else {
+    element.classList.add("wrong");
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove("correct");
+  element.classList.remove("wrong");
+}
+
+const questions = [
+  {
+    question: "/images/pbs.png",
+    answers: [
+      { text: "PBS", correct: true },
+      { text: "TNT", correct: false },
+      { text: "ABC", correct: false },
+      { text: "VH1", correct: false },
+    ],
+  },
+  {
+    question: "/images/mc.png",
+    answers: [
+      { text: "Visa", correct: false },
+      { text: "Master Card", correct: true },
+      { text: "American Express", correct: false },
+      { text: "Citi", correct: false },
+    ],
+  },
+  {
+    question: "/images/tt.png",
+    answers: [
+      { text: "HBO", correct: false },
+      { text: "Spotify", correct: false },
+      { text: "Myspace", correct: false },
+      { text: "Twitter", correct: true },
+    ],
+  },
+  {
+    question: "/images/wiki.jpeg",
+    answers: [
+      { text: "Grammerly", correct: false },
+      { text: "MIT", correct: false },
+      { text: "Wikepdia", correct: true },
+      { text: "National Geographic", correct: false },
+    ],
+  },
+];
+
+function loadScore() {
+  const totalScore = document.getElementById("score");
+  totalScore.textContent = `You scored ${score} out of ${Questions.length}`;
+}
